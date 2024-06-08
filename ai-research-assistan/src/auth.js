28e16f67e40938c9c739097bf9a91a6a9767c9ee
Adrián Pabón Mendoza <afpabon@uninorte.edu.co>
2024-06-08 12:18:46 -0500

    botón de google

--- ai-research-assistan/src/auth.js
+++ ai-research-assistan/src/auth.js
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { FcGoogle } from 'react-icons/fc'; // Importando el icono de Google
 
 const AuthContext = createContext(null);
 
@@ -31,7 +32,20 @@ function Auth() {
     }
   };
 
-  return user ? <div>Logged in as {user.email}</div> : <button onClick={handleLogin}>Log in with Google</button>;
+  return (
+    <div className="flex justify-center items-center h-screen"> {/* Centro el botón en la pantalla */}
+      {user ? (
+        <div>Logged in as {user.email}</div>
+      ) : (
+        <button 
+          onClick={handleLogin}
+          className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded inline-flex items-center"
+        >
+          <FcGoogle className="mr-2" /> Log in with Google
+        </button>
+      )}
+    </div>
+  );
 }
 
 export const AuthProvider = ({ children }) => {
